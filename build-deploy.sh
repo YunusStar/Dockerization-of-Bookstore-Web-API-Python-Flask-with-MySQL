0741c44cf97a51d26bc41c12c0ebdbee1cca624f
YunusStar <dolphinstar121@gmail.com>
2021-06-11 08:45:53 +0300

    template added

--- build-deploy.sh
+++ build-deploy.sh
@@ -8,8 +8,8 @@ curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compo
 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 mkdir -p /home/ec2-user/bookstore-api
-TOKEN="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-FOLDER="https://$TOKEN@raw.githubusercontent.com/clarusway/cw-workshop/dev/devops/projects/202-dockerization-bookstore-api-on-python-flask-mysql/"
+TOKEN="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"
+FOLDER="https://raw.githubusercontent.com/YunusStar/Dockerization-of-Bookstore-Web-API-Python-Flask-with-MySQL/main/Dockerfile"
 curl -s --create-dirs -o "/home/ec2-user/bookstore-api/app.py" -L "$FOLDER"bookstore-api.py
 curl -s --create-dirs -o "/home/ec2-user/bookstore-api/requirements.txt" -L "$FOLDER"requirements.txt
 curl -s --create-dirs -o "/home/ec2-user/bookstore-api/Dockerfile" -L "$FOLDER"Dockerfile
